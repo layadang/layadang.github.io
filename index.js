89f80a66ad9bda0fd0cd395ec31db1728309d88c
layadang <pd03@bu.edu>
2023-04-20 13:13:16 -0400

    language toggle in about page

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function toggleLanguage(lang) {
+    var english = document.getElementById("english");
+    var vietnamese = document.getElementById("vnmese");
+
+    if (lang === "vi") {
+      english.style.display = "none";
+      vietnamese.style.display = "block";
+    } else {
+      english.style.display = "block";
+      vietnamese.style.display = "none";
+    } 
+
+  }
